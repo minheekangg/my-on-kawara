1b800609904fee8439deb22143e67828aa66c462
minheekangg <minheekangg@gmail.com>
2020-03-21 20:31:15 -0400

    added articles

---
+++ client/src/components/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Parallax from './Parallax';
+
+function App() {
+    return (
+        <div>
+            <Parallax />
+        </div>
+    );
+}
+export default App;
