877677a3388506f5380b60e604761ac332a996b0
minheekangg <minheekangg@gmail.com>
2020-03-22 22:21:48 -0400

    pull images from upsplash

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,26 +1,39 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 
+const searchTerm = 'paris';
+const upsplash_id = process.env.REACT_APP_UPSPLASH_ACCESS_KEY.replace("'","");
+
 export const App = () => {
   const [articles, setArticles] = useState();
 
   useEffect(() => {
     axios
-      .get(
-        "http://localhost:8000/api/articles/"
-      )
-      .then( res => {
-          if (res && res.data && res.data.articles) {
-              setArticles(res.data.articles);
-          }
-      })
-      .catch(err=>console.log('error', err));
+        .get(`https://api.unsplash.com/search/photos`, {
+            params: { query: searchTerm },
+            headers: {
+                Authorization:
+                    `Client-ID ${upsplash_id}`
+            }
+        })
+        .then(res => {
+            if (res && res.data && res.data.results && !!res.data.results.length){
+                console.log(res.data.results);
+                setArticles(res.data.results);
+            }
+        })
+        //   .then( res => {
+        //       if (res && res.data && res.data.articles) {
+        //           setArticles(res.data.articles);
+        //       }
+        //   })
+        .catch(err => console.log("error", err));
   }, []);
 
   return articles ? (
       <div>
           {articles.map(article => {
-              return <div>{article.title}</div>
+              return <img src={article.urls.small} key={article.id} alt={article.alt_description}/>
           })}
       </div>
   ) : (
