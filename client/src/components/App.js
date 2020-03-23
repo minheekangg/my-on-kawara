67cdc2347b5d3074b8b62ee83a10f6000e4a2612
minheekangg <minheekangg@gmail.com>
2020-03-22 20:15:29 -0400

    client added app.js

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,11 +1,32 @@
-import React from 'react';
-import Parallax from './Parallax';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
-function App() {
-    return (
-        <div>
-            <Parallax />
-        </div>
-    );
-}
+export const App = () => {
+  const [articles, setArticles] = useState();
+//   const [nextTodoId, setNextTodoId] = useState(0);
+//   const [newTodoLabel, setNewTodoLabel] = useState("");
+
+  useEffect(() => {
+    axios
+      .get(
+        "http://localhost:8000/api/articles/"
+      )
+      .then( ({articles})=> {
+          console.log(articles);
+        setArticles(articles);
+      })
+      .catch(err=>console.log('error', err));
+  }, []);
+
+  return articles ? (
+      <div>
+          {articles.map(article => {
+              return <div>{article.title}</div>
+          })}
+      </div>
+  ) : (
+      <div>nothing yet</div>
+  )
+};
+
 export default App;
