f5163adc144dc2355cccfcef0a34bf0c7e749238
minheekangg <minheekangg@gmail.com>
2020-05-03 22:50:45 -0400

    create loop to get dates option

--- client/src/components/create-article/create-destinations.js
+++ client/src/components/create-article/create-destinations.js
@@ -1,30 +1,45 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 // import styled from "styled-components";
 import moment from "moment";
+import { Dropdown, Form, Select, Input } from "semantic-ui-react";
+
+const options = [];
 
 const CreateDestinations = (props) => {
     const [days, setDays] = useState(props.days);
     const [endDate, setEndDate] = useState(props.end);
     const [startDate, setStartDate] = useState(props.start);
     const [destinations, setDestinations] = useState([""]);
+    const [daysOptions, setDaysOptions] = useState([]);
 
-    const formatDays = () => {
-        console.log('props are', props)
-        var daysArr = [];
-        var counter = 0;
-        var day = moment(startDate, "MM/DD/YYYY");
-        // var end = moment(endDate, "MM/DD/YYYY");
+    useEffect(() => {
+        function formatDays(){
+            console.log("props are", props);
+            var daysArr = [];
+            var counter = 0;
+            var day = moment(startDate).format("MM/DD/YYYY");
+            // var end = moment(endDate, "MM/DD/YYYY");
 
-        while (counter <= days) {
-            console.log("day is", day);
-            daysArr.push(day);
-            day = moment(day).add(1, "days").format("MM/DD/YYYY");
-            counter++;
-        }
-        debugger
-        console.log("days are", daysArr);
-        return daysArr;
-    }
+            while (counter <= days) {
+                console.log("day is", day);
+                daysArr.push({
+                    key: day,
+                    text: day,
+                    value: day,
+                });
+
+                day = moment(day).add(1, "days").format("MM/DD/YYYY");
+
+                counter++;
+            }
+
+            console.log("days are", daysArr);
+            return setDaysOptions(daysArr);
+        };
+
+        formatDays();
+    });
+    
 
     const handleSubmit = () => {
         // if (!!formValidation()) {
@@ -43,7 +58,16 @@ const CreateDestinations = (props) => {
 
     return (
         <form onSubmit={handleSubmit}>
-            {formatDays()} days
+            {days} days
+            <Form.Field control={Input} label="City" placeholder="City" />
+            {daysOptions.length > 0 && (
+                <Form.Field
+                    control={Select}
+                    label="Gender"
+                    options={options}
+                    placeholder="Gender"
+                />
+            )}
             <input type="submit" value="Submit" />
         </form>
     );
