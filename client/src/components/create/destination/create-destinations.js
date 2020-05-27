4c7085c29536ca8c78cef28cb239b8022efb6d34
minheekangg <minheekangg@gmail.com>
2020-05-26 23:12:30 -0400

    flexible destinations set

--- client/src/components/create/destination/create-destinations.js
+++ client/src/components/create/destination/create-destinations.js
@@ -6,9 +6,11 @@ import { Form, Select, Input, Button } from "semantic-ui-react";
 
 const CreateDestinations = (props) => {
     const [days , setDays] = useState(props.days);
-    const [destination1, setDestination1] = useState();
-    const [destination2, setDestination2] = useState();
-    const [destination3, setDestination3] = useState();
+    const [destinations, setDestinations] = useState([{
+        city: "",
+        startDate: "",
+        endDate: "",
+    }]);
 
     const formatDays = (days) => {
         if (!days || days.length <= 0 ) {
@@ -24,17 +26,12 @@ const CreateDestinations = (props) => {
         })
     }
 
-    const handleChange = (value, key, num)=> {
-        let newInput = {};
-        newInput[key] = value;
-        
-        if (num === 1) {
-            setDestination1(prev =>({...prev, ...newInput}));
-        } else if (num === 2) {
-            setDestination2(prev => ({ ...prev, ...newInput }));
-        } else if (num === 3) {
-            setDestination3(prev => ({ ...prev, ...newInput }));
-        }
+    const handleChange = (value, key, idx)=> {
+        let newDestinations = [...destinations];
+        let current = newDestinations[idx];
+        current[key] = value;
+        setDestinations(newDestinations);
+        return;
     }
 
     const handleSubmit = (e) => {
@@ -52,58 +49,32 @@ const CreateDestinations = (props) => {
 
     return (
         <Form onSubmit={handleSubmit}>
-            <Form.Field
-                control={Input}
-                label="City"
-                onChange={(e, { value }) => handleChange(value, "city", 1)}
-            />
-            <Form.Field
-                control={Select}
-                onChange={(e, { value }) => handleChange(value, "startDate", 1)}
-                label="Start Date"
-                options={formatDays(days)}
-            />
-            <Form.Field
-                control={Select}
-                label="End Date"
-                options={formatDays(days)}
-                onChange={(e, { value }) => handleChange(value, "endDate", 1)}
-            />
-            <Form.Field
-                control={Input}
-                label="City"
-                placeholder="City"
-                onChange={(e, { value }) => handleChange(value, "city", 2)}
-            />
-            <Form.Field
-                control={Select}
-                label="Start Date"
-                options={formatDays(days)}
-                onChange={(e, { value }) => handleChange(value, "startDate", 2)}
-            />
-            <Form.Field
-                control={Select}
-                label="End Date"
-                options={formatDays(days)}
-                onChange={(e, { value }) => handleChange(value, "endDate", 2)}
-            />
-            <Form.Field
-                control={Input}
-                label="City"
-                onChange={(e, { value }) => handleChange(value, "city", 3)}
-            />
-            <Form.Field
-                control={Select}
-                label="Start Date"
-                options={formatDays(days)}
-                onChange={(e, { value }) => handleChange(value, "startDate", 3)}
-            />
-            <Form.Field
-                control={Select}
-                label="End Date"
-                options={formatDays(days)}
-                onChange={(e, { value }) => handleChange(value, "endDate", 2)}
-            />
+                {
+                    destinations.map((d, idx) => {
+                        return <Form.Group key={`destination-${idx}`}>
+                            <Form.Field
+                                control={Input}
+                                label="City"
+                                name="city"
+                                value={d.city}
+                                onChange={(e) => handlePeopleChange(e.target.value, "city", idx)}
+                            />
+                            <Form.Field
+                                control={Select}
+                                onChange={(e, { value }) => handleChange(value, "startDate", idx)}
+                                label="Start Date"
+                                options={formatDays(days)}
+                            />
+                            <Form.Field
+                                control={Select}
+                                label="End Date"
+                                options={formatDays(days)}
+                                onChange={(e, { value }) => handleChange(value, "endDate", idx)}
+                            />
+                        </Form.Group>
+                    })
+                }
+            <Button onClick={() => setDestinations(destination.concat({ city: "", startDate: "", endDate: "" }))}>+</Button>
             <Form.Button>Submit</Form.Button>
         </Form>
     );
