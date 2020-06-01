ed691baa47c77180b16160d34be7f7ca49bab480
minheekangg <minheekangg@gmail.com>
2020-06-01 12:49:01 -0400

    created articles list

--- client/src/components/articles/articles.js
+++ client/src/components/articles/articles.js
@@ -1,21 +1,62 @@
 import React, { useEffect } from "react";
+import styled from "styled-components";
 
-// import PostTitle from '../show-article/article-title/title';
-// import PictureContainer from '../show-article/picture-container';
+const StyledWrapper = styled.div`
+    max-width: 300px;
+    margin: auto;
+    height: 100vh;
+    display: flex;
+    align-items: center;
+    `;
+    
+const StyledArticlesWrapper = styled.div`
+    // background-color: #f2eade;
+    display: flex;
+    border: 1px solid;
+    flex-direction: column;
+    padding: 10px;
+    width: 100%;
+
+    h2 {
+        border-bottom: 1px dotted;
+    }
+
+`;
+    
+const ArticleWrapper = styled.div`
+    display: flex;
+    justify-content: space-between;
+    // border: 1px solid blue;
+    min-width: 100%;
+    margin: 10px 0;
+    cursor: pointer;
+
+    &:hover {
+        color: red;
+    }
+`;
+
 
 const Articles = (props) => {
     const { fetchArticles } = props;
-    // const { articles } = props;
+    const { articles } = props;
 
     useEffect(() => {
         fetchArticles();
     }, [fetchArticles]);
 
     return props.articles && !!props.articles.length ? (
-        <div>
-            {/* <PostTitle article={articles[articles.length -1]} />
-            <PictureContainer article={articles[articles.length -1]} /> */}
-        </div>
+        <StyledWrapper>
+            <StyledArticlesWrapper>
+                <h2>Travel</h2>
+            {articles.map(e=>{
+                return <ArticleWrapper onClick={()=>props.history.push(`articles/${e._id}`)}>
+                    <div>{e.title}</div>
+                    <div>{e.startDate} - {e.endDate}</div>
+                </ArticleWrapper>
+            })}
+            </StyledArticlesWrapper>
+        </StyledWrapper>
     ) : (
         <div>nothing yet</div>
     );
