cef7a3ae7d8753ba1c57f2b4efae556d72f4ebe1
minheekangg <minheekangg@gmail.com>
2020-06-08 22:24:46 -0400

    make it work in local

--- app.js
+++ app.js
@@ -58,12 +58,12 @@ if (!isProduction) {
     app.use(errorHandler());
 } 
 
-// Serve any static files
-app.use(express.static(path.join(__dirname, "client/build")));
-// Handle React routing, return all requests to React app
-app.get("*", function (req, res) {
-    res.sendFile(path.join(__dirname, "client/build", "index.html"));
-});
+// // Serve any static files
+// app.use(express.static(path.join(__dirname, "client/build")));
+// // Handle React routing, return all requests to React app
+// app.get("*", function (req, res) {
+//     res.sendFile(path.join(__dirname, "client/build", "index.html"));
+// });
 
 
 // Add models
