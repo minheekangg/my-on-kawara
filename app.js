73bbc0599433e3653614f6e970b4bf7614b39b04
minheekangg <minheekangg@gmail.com>
2020-06-08 21:58:18 -0400

    use build;

--- app.js
+++ app.js
@@ -56,14 +56,15 @@ app.use(
 
 if (!isProduction) {
     app.use(errorHandler());
-} else {
-    // Serve any static files
-    app.use(express.static(path.join(__dirname, "client/build")));
-    // Handle React routing, return all requests to React app
-    app.get("*", function (req, res) {
-        res.sendFile(path.join(__dirname, "client/build", "index.html"));
-    });
-}
+} 
+
+// Serve any static files
+app.use(express.static(path.join(__dirname, "client/build")));
+// Handle React routing, return all requests to React app
+app.get("*", function (req, res) {
+    res.sendFile(path.join(__dirname, "client/build", "index.html"));
+});
+
 
 // Add models
 require('./models/Trip');
