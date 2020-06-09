09967249aee017d2a32a122830e083b56a44da06
minheekangg <minheekangg@gmail.com>
2020-06-09 17:22:44 -0400

    remove build from gitignore

---
+++ client/build/precache-manifest.1e6dbdffd7ed89edb2ff1a9ea0c11b6f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a852adf23e97c69c6fa85118317cd35e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "be59a44e133d8e2cde47",
+    "url": "/static/css/2.ecffae5e.chunk.css"
+  },
+  {
+    "revision": "69fe0c53b0edb69193b5",
+    "url": "/static/css/main.fd6669e8.chunk.css"
+  },
+  {
+    "revision": "be59a44e133d8e2cde47",
+    "url": "/static/js/2.97cf975c.chunk.js"
+  },
+  {
+    "revision": "c3b8755184206352e6870827ce715a1e",
+    "url": "/static/js/2.97cf975c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "69fe0c53b0edb69193b5",
+    "url": "/static/js/main.14f72bec.chunk.js"
+  },
+  {
+    "revision": "13c5ce7c89dba05c4fba",
+    "url": "/static/js/runtime-main.8c40394c.js"
+  }
+]);
