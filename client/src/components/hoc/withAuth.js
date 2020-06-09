defdff3f0bc09d290574b77ed9a0624a4c37b7c5
minheekangg <minheekangg@gmail.com>
2020-06-08 21:39:40 -0400

    create/edit limit to only if admin

--- client/src/components/hoc/withAuth.js
+++ client/src/components/hoc/withAuth.js
@@ -1,10 +1,17 @@
 import React from "react";
-import { connect } from "react-redux";
-import { Redirect } from "react-router";
-// import { fetchCurrentUser } from "../actions/userActions";
-import { Loader } from "semantic-ui-react";
+import styled from "styled-components";
+import { Link } from "react-router-dom";
+import { Loader, Button, Icon } from "semantic-ui-react";
 
 const CLIENT_ID = process.env.REACT_APP_OAUTH;
+const ADMIN_EMAIL = process.env.REACT_APP_ADMIN_EMAIL;
+
+const StyledWrapper = styled.div`
+    display: flex;
+    height: 100vh; 
+
+    .ui.buttons {margin:auto}
+`;
 
 const withAuth = (WrappedComponent) => {
     class AuthorizedComponent extends React.Component {
@@ -17,46 +24,53 @@ const withAuth = (WrappedComponent) => {
                     scope: 'email'
                 }).then(()=> {
                     this.auth = window.gapi.auth2.getAuthInstance();
-                    this.setState({isSignedIn: this.auth.isSignedIn.get()});
+                    this.onAuthChange();
                     this.auth.isSignedIn.listen(this.onAuthChange);
                 })
             })
         }
 
         onAuthChange = () => {
-            this.setState({isSignedIn: this.auth.isSignedIn.get() })
+            this.setState({ isSignedIn: this.auth.isSignedIn.get() })
+        }
+
+        onSignIn = () => {
+            this.auth.signIn();
+        }
+
+        renderSignInOrReturn = () => {
+            return <StyledWrapper>
+                <Button.Group>
+                    <Button icon negative labelPosition='left' onClick={this.onSignIn}>
+                        <Icon name='google' />
+                            Sign in with Google
+                        </Button>
+                    <Button.Or />
+                    <Button as={Link} to="/articles" icon labelPosition='right'>
+                        Return to Articles
+                        <Icon name='right arrow' />
+                    </Button>
+                </Button.Group>
+            </StyledWrapper>
         }
 
         render() {
             if ( this.state.isSignedIn === null ) {
-                return null;
+                return <Loader active inline="centered" />;
             } else if (this.state.isSignedIn) {
-                return <WrappedComponent {...this.props} />
+                if ( this.auth.currentUser.get().getBasicProfile().getEmail() === ADMIN_EMAIL) {
+                    return <WrappedComponent {...this.props} />
+                } else {
+                   return this.renderSignInOrReturn();
+                }
             } else {
-                return <div>should sign in</div>
+                return this.renderSignInOrReturn();
             }
-            // // console.log('%c INSIDE RENDER FOR HOC', 'color: green')
-            // if (localStorage.getItem('jwt') && this.props.loggedIn) {
-            //     
-            // } else if (localStorage.getItem('jwt') && (this.props.authenticatingUser || !this.props.loggedIn)) {
-            //     return <Loader active inline="centered" />
-            // } else {
-            //     return <Redirect to="/login" />
-            // }
-            return <WrappedComponent {...this.props} />
         }
     }
 
-    const mapStateToProps = (state) => {
-        return {
-            // loggedIn: state.user.isLoggedIn,
-            // authenticatingUser: state.user.authenticatingUser
-        }
-    }
 
-    // const mapDispatchToProps = { fetchCurrentUser: fetchCurrentUser }
-
-return connect(mapStateToProps)(AuthorizedComponent)
+return AuthorizedComponent
 }
 
 export default withAuth
