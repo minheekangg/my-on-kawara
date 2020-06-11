39da1844148dd839c2a908a62459f36778600f2a
minheekangg <minheekangg@gmail.com>
2020-06-11 15:58:21 -0400

    build done

---
+++ client/build/precache-manifest.8d0163be0eae0c482afc4330465d078b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b1854650e51c1ac0266a512cd5309a90",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4cf4d420e3f87bc7b7cf",
+    "url": "/static/css/2.ecffae5e.chunk.css"
+  },
+  {
+    "revision": "15f109baca5e51487987",
+    "url": "/static/css/main.fd6669e8.chunk.css"
+  },
+  {
+    "revision": "4cf4d420e3f87bc7b7cf",
+    "url": "/static/js/2.f0021dda.chunk.js"
+  },
+  {
+    "revision": "c3b8755184206352e6870827ce715a1e",
+    "url": "/static/js/2.f0021dda.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "15f109baca5e51487987",
+    "url": "/static/js/main.e34dbaa2.chunk.js"
+  },
+  {
+    "revision": "13c5ce7c89dba05c4fba",
+    "url": "/static/js/runtime-main.8c40394c.js"
+  }
+]);
